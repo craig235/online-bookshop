2de690d83ffb54f9d3d0fe60de0409cf0c2eb4f5
craig235 <crawells@gmail.com>
2022-10-26 12:02:43 +0100

    initial commit

---
+++ src/test/java/com/qa/bookshop/OnlineBookshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qa.bookshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineBookshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
