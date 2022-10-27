5f853fe943aa6bcaadad6e4ee0ffb40f55e20d84
craig235 <crawells@gmail.com>
2022-10-27 17:29:32 +0100

    cleanup

--- src/main/java/com/qa/bookshop/controller/AuthController.java
+++ src/main/java/com/qa/bookshop/controller/AuthController.java
@@ -3,7 +3,6 @@ package com.qa.bookshop.controller;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -11,23 +10,22 @@ import org.springframework.web.bind.annotation.*;
 import com.qa.bookshop.entity.User;
 import com.qa.bookshop.exception.InvalidCredentialsException;
 import com.qa.bookshop.exception.UserAlreadyExistsException;
-import com.qa.bookshop.service.LoginService;
-import com.qa.bookshop.service.LoginServiceImpl;
+import com.qa.bookshop.service.UserService;
 
 @RestController
-@RequestMapping("api/v1")
-public class LoginController {
+@RequestMapping("api/v1/auth-service")
+public class AuthController {
 
     @Autowired
     //@Qualifier("LoginServiceImpl")
-    LoginService loginService;
+    UserService userService;
 
     ResponseEntity<?> responseEntity;
 
-    @PostMapping("/user/login")
+    @PostMapping("login")
     public ResponseEntity<?> login(@Valid @RequestBody User user) throws InvalidCredentialsException {
         try {
-            User userRecord = this.loginService.login(user);
+            User userRecord = this.userService.login(user);
             responseEntity = new ResponseEntity<>(userRecord, HttpStatus.OK);
         } catch (InvalidCredentialsException e) {
             throw e;
@@ -38,10 +36,10 @@ public class LoginController {
         return responseEntity;
     }
 
-    @PostMapping("/user/signup")
+    @PostMapping("signup")
     public ResponseEntity<?> signup(@Valid @RequestBody User user) throws UserAlreadyExistsException {
         try {
-            User userRecord = this.loginService.signup(user);
+            User userRecord = this.userService.save(user);
             System.out.println("Added user: " + userRecord);
             responseEntity = new ResponseEntity<>(userRecord, HttpStatus.CREATED);
         } catch (UserAlreadyExistsException e) {
--- src/main/java/com/qa/bookshop/service/UserServiceImpl.java
+++ src/main/java/com/qa/bookshop/service/UserServiceImpl.java
@@ -5,12 +5,11 @@ import com.qa.bookshop.exception.InvalidCredentialsException;
 import com.qa.bookshop.exception.UserAlreadyExistsException;
 import com.qa.bookshop.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
 //@Component("LoginServiceImpl")
-@Service("LoginServiceImpl")
-public class LoginServiceImpl implements LoginService {
+@Service("UserServiceImpl")
+public class UserServiceImpl implements UserService {
 
     @Autowired
     UserRepository userRepository;
@@ -31,14 +30,14 @@ public class LoginServiceImpl implements LoginService {
     }
 
     @Override
-    public User signup(User user) throws UserAlreadyExistsException {
+    public User save(User user) throws UserAlreadyExistsException {
         User userRecord = this.userRepository.findByUsername(user.getUsername());
         if(userRecord != null) {
             throw new UserAlreadyExistsException();
         }
 
         userRecord = this.userRepository.save(user);
-        userRecord.setPassword("SignupOK");
+        userRecord.setPassword("");
         return userRecord;
     }
 }
